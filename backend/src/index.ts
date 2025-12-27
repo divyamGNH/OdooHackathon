0d2d63e3144c58ced8bce38a25d72f84c0c62780
divyam <divyam.iiitv@gmail.com>
2025-12-27 12:56:40 +0530

    sync repo to divyam local code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,10 +13,9 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 
